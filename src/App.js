15906d313e5abd2575423880f6a3328bcffcb19c
BarelyBrian <brian.airbnb.bb@gmail.com>
2026-03-11 13:22:07 +0300

    signup

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import SignUp from './components/SignUp';
 import AddProduct from './components/AddProduct';
 import GetProduct from './components/GetProducts';
 import MpesaPayment from './components/MpesaPayment';
-import { BrowserRouter,Routes,Route  } from 'react-router';
-
+import { BrowserRouter,Routes,Route } from 'react-router';
+import NavBar from './components/NavBar1';
 function App() {
   return (
     <BrowserRouter>
@@ -15,13 +15,14 @@ function App() {
       <header className="App-header bg-info     ">
       <h1 className="text-center p-3 fw-bold">SokoGarden- Buy and Sell Online</h1>
       </header>
+    <NavBar/>
       {/* {Create the routes} */}
       <Routes>
         {/* map a single route */}
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/addproduct' element={<AddProduct/>}/>
-        <Route path='/getproduct' element={<GetProduct/>}/>
+        <Route path='/' element={<GetProduct/>}/>
         <Route path='/mpesapayment' element={<MpesaPayment/>}/>
       </Routes>
     </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from "react-router";
 const SignUp = () =>{
     return(
-        <div>
-            <h1>Welcome to the Sign Up page</h1> 
+        <div className="d-flex justify-content-center row text-center">
+            <div className="col-md-6 p-2 mt-4 card shadow">
+                <h1 className="text-center p-3">Sign Up Page</h1>
+                <form action="">
+                    <input type="text" placeholder="Enter your username"className="form-control"required/><br />
+                    <input type="email" placeholder="Enter your email"className="form-control"required/><br />
+                    <input type="password" placeholder="Enter your password"className="form-control"required/><br />
+                    <input type="text" placeholder="Enter your phone number"className="form-control"required/><br />
+                    <button type="submit"className="btn btn-info">Sign Up</button>
+                    {/* create a link that asks you whether you have an account */}
+                    <p>Already have an account? <Link to="/signin">Sign In</Link></p>
+                </form>
+            </div>
         </div>
     )
 }
