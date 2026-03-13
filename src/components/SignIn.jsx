b339c5c61207d776ac0370ad42b961966d202c71
BarelyBrian <brian.airbnb.bb@gmail.com>
2026-03-13 16:03:42 +0300

    f

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import axios from "axios"
+import { useState } from "react"
+import { Link } from "react-router"
 const SignIn = () =>{
+    const [username,setUsername]=useState("")
+    const[password,setPassword]=useState("")
+    //
+    const[loading,setLoading]=useState("")
+    const[success,setSuccess]=useState("")
+    const[error,setError]=useState("")
+    const submit = async(e)=>{
+        e.preventDefault()
+        setLoading("Please wait...")
+        try {
+            const data= new FormData()
+            data.append('username',username)
+            data.append('password',password)
+            // wait for response from back end
+            const response= await axios.post("http://brianswala.alwaysdata.net/api/signin",data)
+            // the fields should be empty after submitting data
+            setLoading("")
+            setSuccess(response.data.message)
+            // clear the form fields
+                setUsername("")
+                setPassword("")
+        } catch (error) {
+            setLoading('')
+            setError(error.message)
+            
+        }
+    }
     return(
-        <div>
-            <h1>Welcome to the Sign In page</h1>
+        <div className="d-flex text-center justify-content-center row">
+            <div className="col-md-6 card shadow">
+                {success}
+                {loading}
+                {error}
+                <h1>Sign In</h1>
+                <form onSubmit={submit}>
+                    <input type="text" placeholder="username"className="form-control" required value={username} onChange={(e)=>setUsername(e.target.value)}/><br />{username}
+                    <input type="password" placeholder="password" className="form-control" required value={password} onChange={(e)=>setPassword(e.target.value)}/><br />{password}
+                    <button type="submit" className="btn btn-info"> Sign In</button>
+                    <p>Don't have an account?</p> <Link to="/signup">Sign Up</Link>
+                </form>
+            </div>
         </div>
     )
 }
